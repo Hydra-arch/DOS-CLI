a04780888934f4c531dc44ea5b7a6e7903d00c39
Hydra-arch <129088575+Hydra-arch@users.noreply.github.com>
2024-07-19 16:44:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade && sudo apt install python3 && sudo apt install hping3 && pip3 install colorama && pip3 install pyfiglet
+sudo apt update -y && sudo apt upgrade -y && sudo apt install python3 -y && sudo apt install hping3 -y && pip3 install colorama && pip3 install pyfiglet
